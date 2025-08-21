a42364017e92f2b80e23fa4d202e5f203a942dc0
Nguyen Thanh Tung <tungnguyenthanh710@gmail.com>
2025-08-21 13:00:28 +0700

    aaa

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  @Render("home.ejs")
+  handleHomePage() {
+    const message = this.appService.getHello();
+
+    return {
+      message: message
+    }
+    // return "this.appService.getHello()";
+  }
+}
